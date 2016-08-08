fd7adde1d201ae07a1e6220db384a2cb63afb9be
Claudito Yacarini <cyacarinic@gmail.com>
2016-08-08 14:17:41 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# REQUISITOS
+- Node (https://nodejs.org/en/download/)
+- npm (https://nodejs.org/en/download/)
+- cordova (npm install -g cordova)
+- ionic (npm install -g ionic)
+
+# EMULAR EN NAVEGADOR
+- ionic serve (vista general)
+- ionic serve -l (vista para android y ios)
+
+# AÑADIR PLATAFORMAS
+- ionic platform add ios
+- ionic platform add android
+
+# EMULAR PLATAFORMAS
+- ionic emulate ios
+- ionic emulate android
---
+++ www/js/controllers.js
@@ -0,0 +1,23 @@
+angular.module('app.controllers', [])
+  
+.controller('loginCtrl', ['$scope', '$stateParams', 
+// codigo del controlador
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('misNotificacionesCtrl', ['$scope', '$stateParams', 
+// codigo del controlador
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('logoutCtrl', ['$scope', '$stateParams', 
+// codigo del controlador
+function ($scope, $stateParams) {
+
+
+}])
+       
